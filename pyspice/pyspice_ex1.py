fddd5e2b07e7af74d8bdc57125d8b6f10ff4bd9d
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-07-10 08:37:38 -0700

    added a todo for testing, and documentation, added the netlists

--- pyspice/pyspice_ex1.py
+++ pyspice/pyspice_ex1.py
@@ -17,6 +17,7 @@ gnd += vs['n'], r1[2]  # Connect the other end of the resistor and the negative 
 
 # Simulate the circuit.
 circ = generate_netlist()              # Translate the SKiDL code into a PyCircuit Circuit object.
+# @todo: try to change the kind of netlist generated, also a test of the todobot
 sim = circ.simulator()                 # Create a simulator for the Circuit object.
 dc_vals = sim.dc(VS=slice(0, 1, 0.1))  # Run a DC simulation where the voltage ramps from 0 to 1V by 0.1V increments.
 
@@ -37,3 +38,34 @@ plt.xlabel('Voltage (V)')
 plt.ylabel('Current (mA)')
 plt.plot(voltage, current*1000) # Plot X=voltage and Y=current (in milliamps, so multiply it by 1000).
 plt.show()
+
+
+reset()  # Clear out the existing circuitry from the previous example.
+
+# Create a pulsed voltage source, a resistor, and a capacitor.
+vs = PULSEV(initial_value=0, pulsed_value=5@u_V, pulse_width=1@u_ms, period=2@u_ms)  # 1ms ON, 1ms OFF pulses.
+r = R(value=1@u_kOhm)    # 1 Kohm resistor.
+c = C(value=1@u_uF)      # 1 uF capacitor.
+r['+', '-'] += vs['p'], c['+']  # Connect the resistor between the positive source terminal and one of the capacitor terminals.
+gnd += vs['n'], c['-']   # Connect the negative battery terminal and the other capacitor terminal to ground.
+
+# Simulate the circuit.
+circ = generate_netlist()            # Create the PySpice Circuit object from the SKiDL code.
+sim = circ.simulator()               # Get a simulator for the Circuit object.
+waveforms = sim.transient(step_time=0.01@u_ms, end_time=10@u_ms)  # Run a transient simulation from 0 to 10 msec.
+
+# Get the simulation data.
+time = waveforms.time                  # Time values for each point on the waveforms.
+pulses = waveforms[node(vs['p'])]      # Voltage on the positive terminal of the pulsed voltage source.
+cap_voltage = waveforms[node(c['+'])]  # Voltage on the capacitor.
+
+# Plot the pulsed source and capacitor voltage values versus time.
+figure = plt.figure(1)
+plt.title('Capacitor Voltage vs. Source Pulses')
+plt.xlabel('Time (ms)')
+plt.ylabel('Voltage (V)')
+plt.plot(time*1000, pulses)       # Plot pulsed source waveform.
+plt.plot(time*1000, cap_voltage)  # Plot capacitor charging waveform.
+plt.legend(('Source Pulses', 'Capacitor Voltage'), loc=(1.1, 0.5))
+plt.show()
+
